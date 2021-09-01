909c45e3cfe71221e738cda08c0f7d3ff4f19b2c
Nikhilnikki22 <89537941+Nikhilnikki22@users.noreply.github.com>
2021-08-31 22:47:12 -0500

    Add files via upload

---
+++ MaddilNlab1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MaddilNlab1;
+
+/**
+ *
+ * @author s543653
+ */
+public class MaddilNlab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a= 35,b=5,sum,sub,div,mul;
+        sum= a+b;
+        sub =a-b;
+        mul =a*b;
+        div =a/b;   
+        System.out.println("sum="+sum+",Difference=" +sub +",Multipication=" +mul + ",Division="+div);
+        
+    }
+    
+}
